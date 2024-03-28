c41d24ae9e42ad357011074b03497280bcdd723e
Rodion Efremov <coderodd3@gmail.com>
2024-03-28 07:55:06 +0200

    Made the project modular.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,9 @@
-module WikiGameKillerFXJava {
+module coderodde.WikiGameKillerFXJava {
+    requires java.desktop;
+    requires java.logging;
+    requires javafx.base;
+    requires javafx.controls;
+    
+    requires coderodde.ThreadPoolBidirectionalBFSPathFinder;
+    requires coderodde.WikipediaGraphNodeExpanders;
 }
