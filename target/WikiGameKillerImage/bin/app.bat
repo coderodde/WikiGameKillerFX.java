defd6e1a29513cea46ac43973278a4de3fbf9011
Rodion Efremov <coderodd3@gmail.com>
2024-03-28 11:15:48 +0200

    Improved button logic.

--- target/WikiGameKillerImage/bin/app.bat
+++ target/WikiGameKillerImage/bin/app.bat
@@ -1,4 +1,4 @@
-#!/bin/sh
-JLINK_VM_OPTIONS=
-DIR=`dirname $0`
-$DIR/java $JLINK_VM_OPTIONS -m coderodde.WikiGameKillerFXJava/com.github.coderodde.wikipedia.game.killer.fx.WikiGameKillerFX "$@"
+@echo off
+set JLINK_VM_OPTIONS=
+set DIR=%~dp0
+"%DIR%\java" %JLINK_VM_OPTIONS% -m coderodde.WikiGameKillerFXJava/com.github.coderodde.wikipedia.game.killer.fx.WikiGameKillerFX %*
